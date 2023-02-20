4e8a518ee9de1d735927d83e77e056b963341481
HoanTV <hoantv@example.com>
2023-02-20 22:53:56 +0700

    #GET_PASSES_THIS_REPO_UDACITY_PLEASE

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -3,11 +3,15 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+connectionCount = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global connectionCount
+    connectionCount += 1
     return connection
 
 # Function to get a post using its ID
@@ -19,8 +23,51 @@ def get_post(post_id):
     return post
 
 # Define the Flask application
+import logging
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s : %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.debug('healthz request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    global connectionCount
+    connection = get_db_connection()
+    postCount = connection.execute('SELECT COUNT(*) FROM posts').fetchone()
+    connection.close()
+    response = app.response_class(
+        response=json.dumps({"db_connection_count": connectionCount, "post_count": postCount[0]}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.debug('metrics request successfull')
+    return response
 
 # Define the main route of the web application 
 @app.route('/')
@@ -36,13 +83,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.debug('Article not found by post id: ' + str(post_id))
       return render_template('404.html'), 404
     else:
+      app.logger.debug('Article "' + post['title'] + '" retrieved!')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.debug('About page is returned!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,6 +111,7 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.debug('Article "' + title + '" is created!')
             return redirect(url_for('index'))
 
     return render_template('create.html')
